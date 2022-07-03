b1b3e98d5ed9dd10ecaa36c7f9a0f4ccc186d68a
David Liberal <davidliberal0@gmail.com>
2022-07-03 17:18:41 -0400

    convert function component to class component

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 import "./css/pokecard.css";
 
-const PokeCard = (props) => {
-  // console.log(props);
-  // const [name, setName] = useState("");
-  // const [img, setimage] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [type, setType] = useState("");
-  // const [dexNum, setDexNum] = useState("");
-
-  return (
-    <section className="rect-section text-white">
-      <div className="rect">
-        <img src={props.pokeImg} alt="" className="legendary-image mr-6" />
-        <div className="rect-name-type mr-6">
-          <h3 className="pokemon-name">{props.pokeName}</h3>
-          <h3 className="dex-number">{props.dexNum}</h3>
-          <div className="type">{props.typeOne}</div>
-          <div className="type-two"></div>
+export default class PokeCard extends React.Component {
+  render() {
+    return (
+      <section className="rect-section text-white">
+        <div className="rect">
+          <img
+            src={this.props.pokeImg}
+            alt=""
+            className="legendary-image mr-6"
+          />
+          <div className="rect-name-type mr-6">
+            <h3 className="pokemon-name">{this.props.pokeName}</h3>
+            <h3 className="dex-number">{this.props.dexNum}</h3>
+            <div className="type">{this.props.typeOne}</div>
+            <div className="type-two"></div>
+          </div>
+          <div className="poke-desc"></div>
         </div>
-        <div className="poke-desc"></div>
-      </div>
-    </section>
-  );
-};
-
-export default PokeCard;
+      </section>
+    );
+  }
+}
