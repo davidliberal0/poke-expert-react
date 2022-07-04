a2237aefb84fa8d45d1edf6399d0f77f5427e953
David Liberal <davidliberal0@gmail.com>
2022-07-04 11:30:49 -0400

    add type color for pokemon type

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import "./css/pokecard.css";
 
-const PokeCard = (props) => {
-  // console.log(props);
-  // const [name, setName] = useState("");
-  // const [img, setimage] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [type, setType] = useState("");
-  // const [dexNum, setDexNum] = useState("");
+// All Possible Types w/ Color Hex Values
+const typeColor = {
+  ground: "#b59469",
+  water: "#0037ff",
+  fire: "#ff2a00",
+  grass: "#0b7026",
+  ghost: "#310273",
+  dragon: "#964ef5",
+  steel: "#8a8888",
+  fairy: "#b569b4",
+  psychic: "#f781ef",
+  normal: "#e3d8ca",
+  fighting: "#800c04",
+  flying: "#c5b8e3",
+  dark: "#0f0f1f",
+  electric: "#ffee00",
+  poison: "#772ff5",
+  ice: "#14bbe0",
+  rock: "#AE964D",
+};
 
+// --------------
+
+const rgbas = {
+  ground: "rgba(181, 148, 105,",
+  water: "rgba(0, 55, 255,",
+  fire: "rgba(255, 42, 0,",
+  grass: "rgba(11, 112, 38,",
+  ghost: "rgba(49, 2, 115,",
+  dragon: "rgba(150, 78, 245,",
+  steel: "rgba(138, 136, 136,",
+  fairy: "rgba(181, 105, 180,",
+  psychic: "rgba(247, 129, 239,",
+  normal: "rgba(227, 216, 202,",
+  fighting: "rgba(128, 12, 4,",
+  flying: "rgba(197, 184, 227,",
+  dark: "rgba(15, 15, 31,",
+  electric: "rgba(255, 238, 0,",
+  poison: "rgba(119, 47, 245,",
+  ice: "rgba(20, 187, 224,",
+  rock: "rgba(174, 150, 77,",
+};
+
+const PokeCard = (props) => {
   return (
     <section className="rect-section text-white">
       <div className="rect">
@@ -16,7 +52,12 @@ const PokeCard = (props) => {
         <div className="rect-name-type mr-6">
           <h3 className="pokemon-name">{props.pokeName}</h3>
           <h3 className="dex-number">{props.dexNum}</h3>
-          <div className="type">{props.typeOne}</div>
+          <div
+            className="type"
+            style={{ backgroundColor: typeColor[props.typeOne] }}
+          >
+            {props.typeOne}
+          </div>
           <div className="type-two"></div>
         </div>
         <div className="poke-desc">{props.desc}</div>
