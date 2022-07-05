b3be125e3625e20f72c54264317b9485fd52c69e
David Liberal <davidliberal0@gmail.com>
2022-07-05 16:03:10 -0400

    fix broken layout on when card is displayed

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -3,18 +3,20 @@ import SearchbarTT from "./SearchbarTT";
 
 function MainSearch() {
   return (
-    <div className="main-content">
-      <div className="container-one flex justify-center items-center flex-col">
-        <div className="heading mt-6 mb-6">
-          <div className='text-white font-bold text-6xl text-center w-auto"'>
-            <span className="text-red-500">Poke</span>Expert
-          </div>
-          <div className="text-white text-6xl font-bold text-center w-auto uppercase tracking-widest mt-3">
-            PokeDex Database
-          </div>
+    <div>
+      <section className="main-content">
+        <div className="container-one flex justify-center items-center flex-col">
+          <section className="heading mt-6 mb-6">
+            <div className='text-white font-bold text-6xl text-center w-auto"'>
+              <span className="text-red-500">Poke</span>Expert
+            </div>
+            <div className="text-white text-6xl font-bold text-center w-auto uppercase tracking-widest mt-3">
+              PokeDex Database
+            </div>
+          </section>
         </div>
-        <SearchbarTT />
-      </div>
+      </section>
+      <SearchbarTT />
     </div>
   );
 }
