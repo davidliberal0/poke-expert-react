e473e198da6db724e38103fef3492a393e47f510
David Liberal <davidliberal0@gmail.com>
2022-07-08 20:55:00 -0400

    add react-router with page routing and fix file tree structure

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div style={{ color: "white" }}>home</div>;
+};
+
+export default Home;
---
+++ src/components/mainSearch/SearchPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import MainSearch from "./MainSearch";
+
+const SearchPage = () => {
+  return (
+    <>
+      <Navbar />
+      <MainSearch />
+    </>
+  );
+};
+
+export default SearchPage;
