78e0e9d19dc7e141bf4597ab8b17010b9f2363a2
davidliberal0 <davidliberal0@gmail.com>
2022-07-11 21:36:46 -0400

    add border to team section and add active navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../css/navbar.css";
 
-function Nabar() {
+const Navbar = () => {
   return (
     <nav className="long-nav bg-black fixed w-full">
       <div className="ml-4 mr-4">
         <div className="sm:flex justify-between">
-          <Link to="/" className="p-3">
+          <NavLink to="/" className="p-3">
             <h1 className="text-3xl text-white font-bold">
               <span className="text-red-500">Poke</span>Expert.
             </h1>
-          </Link>
+          </NavLink>
           <ul className="text-white sm:self-center border-t sm:border-none nav-links text-sm">
             <li className="sm:inline-block" id="link-un">
               <a href="#" className="p-3">
@@ -24,20 +24,39 @@ function Nabar() {
               </a>
             </li>
             <li className="sm:inline-block">
-              <Link to="/database" className="p-3" id="link-selected">
+              <NavLink
+                to="/database"
+                className="p-3"
+                id="link-selected"
+                style={({ isActive }) => {
+                  return {
+                    fontSize: isActive ? "23px" : "",
+                    color: isActive ? "#f15b5e" : "",
+                  };
+                }}
+              >
                 database
-              </Link>
+              </NavLink>
             </li>
             <li className="sm:inline-block" id="link-un">
-              <Link to="/about" className="p-3">
+              <NavLink
+                to="/about"
+                className="p-3"
+                style={({ isActive }) => {
+                  return {
+                    fontSize: isActive ? "23px" : "",
+                    color: isActive ? "#f15b5e" : "",
+                  };
+                }}
+              >
                 About
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
-export default Nabar;
+export default Navbar;
