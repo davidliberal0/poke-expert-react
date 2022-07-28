3d761b6508cba776db0f8af4c9b8a6a0f0445c02
davidliberal0 <davidliberal0@gmail.com>
2022-07-27 23:05:55 -0400

    fix z-index and height issues

--- src/components/homepage/FeaturesSection.jsx
+++ src/components/homepage/FeaturesSection.jsx
@@ -4,20 +4,20 @@ import "../css/featuressection.css"
 const FeaturesSection = () => {
   return (
     <div className='features-section text-white flex flex-row items-center justify-center'>
-        <div className='bullet-land flex flex-col items-center justify-center mr- text-center'>
+        <div className='bullet-land flex flex-col items-center justify-center mr-8 text-center'>
             <div className='rounded-full w-10 h-10 bg-gradient-to-br from-red-800 to-red-500'></div>
-            <div className='uppercase mt-4'>Brainstorm your team</div>
-            <div >Map out your jouney using comprehensive tools</div>
+            <div className='text-2xl uppercase mt-4 font-bold'>Brainstorm</div>
+            <div className='text-lg'>Map out your jouney using comprehensive tools</div>
         </div>
         <div className='bullet-land flex flex-col items-center justify-center mr-8 text-center'>
             <div className='rounded-full w-10 h-10 bg-gradient-to-br from-red-800 to-red-500'></div>
-            <div className='uppercase mt-4'>Search for a pokemon</div>
-            <div>Browse a database of pokemon from generation 1-5</div>
+            <div className='text-2xl uppercase mt-4 font-bold'>Search Pokémon</div>
+            <div className='text-lg'>Browse a database of pokemon from generation 1-5</div>
         </div>
         <div className='bullet-land flex flex-col items-center justify-center text-center'>
             <div className='rounded-full w-10 h-10 bg-gradient-to-br from-red-800 to-red-500'></div>
-            <div className='uppercase mt-4'>Build your team</div>
-            <div>Assemble your team using our team modern team builder</div>
+            <div className='text-2xl uppercase mt-4 font-bold'>Build your team</div>
+            <div className='text-lg'>Assemble your team using our team modern team builder</div>
         </div>
     </div>
   )
--- src/components/homepage/ProductSection.jsx
+++ src/components/homepage/ProductSection.jsx
@@ -4,14 +4,15 @@ import "../css/productsection.css"
 
 const ProductSection = () => {
   return (
-    <div className='product-section flex justify-center relative'>
-      <div className='mt-10 absolute z-10'>
-        <div className='flex items-center justify-center'>
-          <div className='box '></div>
-      </div>
+    <div className='relative'>
+      <div className='product-section flex justify-center relative '>
+        <div className='mt-10 absolute z-10'>
+          <div className='flex items-center justify-center'>
+            <div className='box'></div>
+          </div>
+        </div>
       </div>
     </div>
-
   )
 }
 
