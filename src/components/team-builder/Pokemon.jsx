af8824fba1ce856ca8d323699dcc49f54b09479f
davidliberal0 <davidliberal0@gmail.com>
2022-10-27 20:52:16 -0400

    fix party team member layout with styling

--- src/components/team-builder/Pokemon.jsx
+++ src/components/team-builder/Pokemon.jsx
@@ -5,20 +5,22 @@ import squirtle from "../imgs/squirtle-sprite.png";
 
 const Pokemon = (props) => {
   return (
-    <div className="mb-2 party-item">
-      <img className="team-image-sprite" src={props.img} alt="" />
-      <div className="name-column flex flex-cols">
-        <h1 className="mr-2 text-lg">{props.member}</h1>
-        <div>
-          <FaTimes
-            style={{ color: "red", cursor: "pointer" }}
-            onClick={() => props.onDelete(props.id)}
-          />
+    <div className="wrapper">
+      <div className="mb-2 party-item">
+        <img className="team-image-sprite" src={props.img} alt="" />
+        <div className="name-column ml-2">
+          <h1 className="mr-2 mb-5 mt-5 text-lg">{props.member}</h1>
+          <div className="member-type">{props.type}</div>
+          <div className="member-type-two">{props.typeTwo}</div>
         </div>
-        <div className="member-type">{props.type}</div>
-        <div className="member-type-two"></div>
+        <p className="ml-8">lvl. 100</p>
       </div>
-      <p>lvl. 100</p>
+      <div className="close">
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => props.onDelete(props.id)}
+        />
+      </div>
     </div>
   );
 };
